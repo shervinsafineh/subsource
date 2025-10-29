f71f390f0f73719b528408c431b55dfd0dd4506b
Shervin Safineh <shervinsafinehofficial@gmail.com>
2025-10-30 01:59:30 +0200

    restructure

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -1,9 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Secrets } from '../other/secrets';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Api {
   constructor(private http: HttpClient) {}
+
+  apiKey = Secrets.apiKey;
 }
