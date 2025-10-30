2c3e8c28eb6538865038a8ffc2a80fc18e6944ec
Shervin Safineh <shervinsafinehofficial@gmail.com>
2025-10-30 02:11:49 +0200

    change to headers

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Secrets } from '../other/secrets';
 
@@ -9,5 +9,5 @@ export class Api {
   constructor(private http: HttpClient) {}
 
   baseUrl = 'https://api.subsource.net/api/v1';
-  authString = `X-API-Key: ${Secrets.apiKey}`;
+  headers = new HttpHeaders({ 'X-API-Key': Secrets.apiKey });
 }
